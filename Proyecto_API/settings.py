0cfe72e8d05cda0b394c338d510e281330884ab9
Horacio  Ruiz Diaz <horacioruizd@gmail.com>
2022-10-31 17:22:00 -0300

    make it better

--- Proyecto_API/settings.py
+++ Proyecto_API/settings.py
@@ -148,5 +148,4 @@ os.makedirs(STATIC_ROOT, exist_ok=True)
 STATICFILES_DIR = {
     os.path.join(BASE_DIR, 'static')
 }
-
 staticfiles_storage = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
